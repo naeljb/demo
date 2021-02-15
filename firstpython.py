0dc355003fcb0cbf62c6b40b662d8e20f36214cd
naeljb <79114728+naeljb@users.noreply.github.com>
2021-02-15 16:42:47 -0500

    Create firstpython.py
    this is my first python file in github for testing

---
+++ firstpython.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import math
+import scipy.stats as stats
+
+# how to download a dataset?
+d = "c:/Users/Nael/Documents/dataset.xlsx"
+
+# how to view the first five rows?
+df = pd.read_excel(d)
+df.head()
